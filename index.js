82756257686b054dc005df13a0a9d669598b927c
Alan Verdugo <factorpolar@protonmail.com>
2022-02-17 22:28:07 -0600

    More stuff

--- index.js
+++ index.js
@@ -4,11 +4,38 @@ class Timer {
         this.startButton = startButton;
         this.pauseButton = pauseButton;
 
-        this.startButton.addEventListener('click', this.start)
-    }
+        this.startButton.addEventListener('click', this.start);
+        this.pauseButton.addEventListener('click', this.pause);
+    };
 
     start = () => {
-        console.log("Started timer!");
+        // Jumpstart the timer.
+        this.tick();
+        // Continue executing the tick method every second.
+        this.intervalId = setInterval(this.tick, 1000);
+    };
+
+    pause = () => {
+        clearInterval(this.intervalId);
+    };
+
+    tick = () => {
+        if (this.timeRemaining <= 0) {
+            // Stop counting down if time is up.
+            this.pause();
+        } else {
+            this.timeRemaining = this.timeRemaining - 1;
+        };
+    };
+
+    get timeRemaining() {
+        // time getter.
+        return parseFloat(this.durationInput.value);
+    }
+
+    set timeRemaining(time) {
+        // time setter.
+        this.durationInput.value = time;
     }
 }
 
